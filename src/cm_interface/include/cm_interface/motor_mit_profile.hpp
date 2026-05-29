090784af6dc6d962afce7b6ac454f674368322fc
DurstBuilds <jdurst2000@gmail.com>
2026-05-29 11:47:25 -0400

    stable

--- src/cm_interface/include/cm_interface/motor_mit_profile.hpp
+++ src/cm_interface/include/cm_interface/motor_mit_profile.hpp
@@ -62,9 +62,9 @@ inline constexpr MotorMitProfile kAk80_64{
   -144.0f, 144.0f,
   0.0f, 500.0f,
   0.0f, 5.0f,
-  0.020f, 0.0005f,
+  0.030f, 0.0005f,
   4.0f,
-  40.0f, 0.25f,
+  100.0f, 0.5f,
 };
 
 inline const MotorMitProfile & get_motor_mit_profile(const std::string & motor_model)
